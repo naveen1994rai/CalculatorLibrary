3d4e4a06423a063a661b89086fdfc1ad74ceca57
naveen1994rai <naveenrai973@yahoo.co.in>
2019-08-23 12:10:59 +0800

    Modified test script

--- test_calculator.py
+++ test_calculator.py
@@ -8,7 +8,7 @@ import calculator
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == calculator.addition(2, 2)
+        assert 40 == calculator.addition(20, 20)
 
     def test_subtraction(self):
         assert 2 == calculator.subtraction(4, 2)
